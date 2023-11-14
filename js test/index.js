bbd4f520baeecad5bf98a9bac38ee1488f502d07
Alfat Taufik Nurhidayat <145849640+AlfatTaufik@users.noreply.github.com>
2023-11-14 23:50:32 +0700

    Add files via upload

---
+++ js test/index.js
@@ -0,0 +1,11 @@
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan air");
+// console.log("Mencampurkan air dan kopi");
+// console.log("Menuangkan kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah siap!");
+
+const {coffeeStock, iisCoffeeMachineReady}= require("./state");
+console.log(coffeeStock);
+console.log(iisCoffeeMachineReady);
