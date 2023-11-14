a462afb089c82128201ea730cb41ee2f87d97310
Alfat Taufik Nurhidayat <145849640+AlfatTaufik@users.noreply.github.com>
2023-11-14 23:54:31 +0700

    Add files via upload

---
+++ js test/state.js
@@ -0,0 +1,10 @@
+const coffeeStock = {
+    "arabica" : 100,
+    "alabaca" : 200,
+    "Lampung" : 300
+}
+
+const iisCoffeeMachineReady = true;
+
+module.exports = {coffeeStock, iisCoffeeMachineReady};
+console.log(module);
